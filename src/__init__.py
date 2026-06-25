00dd14e31f56fe0007c9881fa74e90bf5a366c2b
annaschneider162004 <annaschneider162004@gmail.com>
2026-06-25 16:51:41 +0700

    Update src/__init__.py with new modules

--- src/__init__.py
+++ src/__init__.py
@@ -7,10 +7,14 @@ from src.nbt_handler import NBTHandler
 from src.schematic_generator import SchematiGenerator
 from src.city_builder import CityBuilder
 from src.character_manager import CharacterManager
+from src.image_to_schematic import ImageToSchematic
+from src.video_recorder import VideoRecorder
 
 __all__ = [
     "NBTHandler",
     "SchematiGenerator",
     "CityBuilder",
     "CharacterManager",
+    "ImageToSchematic",
+    "VideoRecorder",
 ]
